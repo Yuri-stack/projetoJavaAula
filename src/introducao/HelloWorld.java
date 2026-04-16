85ac8f2491474fe95aa4ff6fb74b51989e2f8d1f
Yuri-stack <yurioliveirasilva1999@gmail.com>
2026-04-16 16:58:55 -0300

    Inicializando meu projeto

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,13 @@
+package introducao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {	// método principal que executda o código
+		
+		// Nossa Lógica Aqui
+		
+		System.out.print("Hello World");	// Identico ao escreva()
+
+	}
+
+}
