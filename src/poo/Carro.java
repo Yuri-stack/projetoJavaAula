b6a33b0b7d75fb81225d943382f6f25823543fbb
Yuri-stack <yurioliveirasilva1999@gmail.com>
2026-04-29 11:46:35 -0300

    Introdução POO Completa

--- src/poo/Carro.java
+++ src/poo/Carro.java
@@ -4,11 +4,53 @@ package poo;
 public class Carro {
 	
 	// Atributos
-	String marca;
-	int rodas;
-	int portas;
-	String modelo_motor;
+	private String marca;
+	private int rodas;
+	private int portas;
+	private String modelo_motor;
 	
+	// Método Construtor
+	public Carro(String marca, int rodas, int portas, String modelo_motor) {
+		this.marca = marca;	//	This => Classe
+		this.rodas = rodas; //	Carro.rodas = Parametro rodas	 
+		this.portas = portas;
+		this.modelo_motor = modelo_motor;
+	}
+	
+	// Métodos de Acesso/Especiais - (Get - Pega / Set - Coloca)
+	public String getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca = marca;	// (This) Essa Classe
+		// Carro.marca = parametro marca
+	}
+	
+	public int getRodas() {
+		return rodas;
+	}
+
+	public void setRodas(int rodas) {
+		this.rodas = rodas;
+	}
+
+	public int getPortas() {
+		return portas;
+	}
+
+	public void setPortas(int portas) {
+		this.portas = portas;
+	}
+
+	public String getModelo_motor() {
+		return modelo_motor;
+	}
+
+	public void setModelo_motor(String modelo_motor) {
+		this.modelo_motor = modelo_motor;
+	}
+
 	// Métodos
 	public void locomover() {
 		System.out.println("Estou me locomovendo...");		
